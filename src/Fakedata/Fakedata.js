1df109925ee173663a8f7395a9ebcbbfafdd86ac
imsamrat <imsamrat24@gmail.com>
2021-05-29 10:17:27 +0600

    Fakedata Completed

---
+++ src/Fakedata/Fakedata.js
@@ -0,0 +1,139 @@
+const Fakedata = [
+	{
+		id: 1,
+        title: 'The Princeton Childrens Book',
+        author: 'by Princeton',
+        Languages: 'English',
+        price: 25,
+        Country: 'USA',
+		img: 'https://i.imgur.com/1nS4irq.png'
+	},
+	{
+		id: 2,
+        title: 'The Golden Treasury of Children Literature',
+        author: 'by Bryna',
+        Languages: 'English',
+        price: 60,
+        Country: 'USA',
+		img: 'https://i.imgur.com/gGPaxHW.png'
+	},
+	{
+		id: 3,
+        title: 'Charlie and the Chocolate Factory',
+        author: 'by Quienten Blake',
+        Languages: 'English',
+        price: 80,
+        Country: 'France',
+		img: 'https://i.imgur.com/YdSzbB7.png'
+	},
+	{
+		id: 4,
+        title: 'The Grimstones',
+        author: 'by Asphyxia',
+        Languages: 'English',
+        price: 40,
+        Country: 'Germany',
+		img: 'https://i.imgur.com/J6p9LmS.png'
+	},
+	{
+		id: 5,
+        title: 'International Childrens Book Day',
+        author: 'by Ibby',
+        Languages: 'English',
+        price: 48,
+        Country: 'Canada',
+		img: 'https://i.imgur.com/e9DcTbO.png'
+	},
+    {
+		id: 6,
+        title: 'Creative Writing with Charlie and the Chocolate Factory',
+        author: 'by Quienten Blake',
+        Languages: 'English',
+        price: 48,
+        Country: 'France',
+		img: 'https://i.imgur.com/HRvvjcN.png'
+	},
+    {
+		id: 7,
+        title: 'Illustrated Stories for Children',
+        author: 'by Usborne',
+        Languages: 'English',
+        price: 60,
+        Country: 'France',
+		img: 'https://i.imgur.com/gRFGjJv.png'
+	},
+    {
+		id: 8,
+        title: 'Through The Yiddish Looking Glass',
+        author: 'by Yiddish',
+        Languages: 'English',
+        price: 70,
+        Country: 'Germany',
+		img: 'https://i.imgur.com/2G5LEnU.png'
+	},
+    {
+		id: 9,
+        title: 'Big Book of Dinosaurs',
+        author: 'by usborne',
+        Languages: 'English',
+        price: 80,
+        Country: 'USA',
+		img: 'https://i.imgur.com/I7RfnOR.png'
+	},
+    {
+		id: 10,
+        title: 'Felix The Cat',
+        author: 'by Felix',
+        Languages: 'English',
+        price: 20,
+        Country: 'England',
+		img: 'https://i.imgur.com/brG6Hbg.png'
+	},
+    {
+		id: 11,
+        title: 'The Comic Book Of History Comics',
+        author: 'by Marvel',
+        Languages: 'English',
+        price: 20,
+        Country: 'England',
+		img: 'https://i.imgur.com/AZSlsnt.png'
+	},
+    {
+		id: 12,
+        title: 'Brightburn',
+        author: 'by ABC',
+        Languages: 'English',
+        price: 30,
+        Country: 'Russia',
+		img: 'https://i.imgur.com/e8VlAMi.png'
+	},
+    {
+		id: 13,
+        title: 'Black Mirror',
+        author: 'by ABC',
+        Languages: 'English',
+        price: 27,
+        Country: 'England',
+		img: 'https://i.imgur.com/yNXIB1V.png'
+	},
+    {
+		id: 14,
+        title: 'Under Preassure',
+        author: 'by Planet Comic',
+        Languages: 'English',
+        price: 27,
+        Country: 'England',
+		img: 'https://i.imgur.com/Pu12bza.png'
+	},
+    {
+		id: 15,
+        title: 'Superhero Comics',
+        author: 'by Chris Gavalar',
+        Languages: 'English',
+        price: 25,
+        Country: 'England',
+		img: 'https://i.imgur.com/3kh9Sdz.png'
+	}
+];
+
+export default Fakedata;
